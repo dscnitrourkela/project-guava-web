63577a2d4f04288a5fd963fe4a28a55c5e95943b
Ritesh Patil <riteshsp2000@gmail.com>
2021-03-17 21:36:42 +0530

    Setup routing using react router dom

--- client/src/config/App.tsx
+++ client/src/config/App.tsx
@@ -3,15 +3,27 @@ import React from 'react';
 // Libraries
 import {ThemeProvider} from '@material-ui/core/styles';
 import {CssBaseline} from '@material-ui/core';
+import {Router, Route, Switch} from 'react-router-dom';
+
+// Components
+import HomePage from '../screens/Home';
 
 // Config
 import theme from './theme';
+
+// Utils
+import createBrowserHistory from '../utils/createBrowserHistory';
 
 function App(): JSX.Element {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <h1>Hello</h1>
+
+      <Router history={createBrowserHistory}>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+        </Switch>
+      </Router>
     </ThemeProvider>
   );
 }
---
+++ client/src/screens/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Home(): JSX.Element {
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+export default Home;
