e6beb86ae66b51372eb84781c54efb7997a121ae
Ritesh Patil <riteshsp2000@gmail.com>
2021-03-17 21:11:16 +0530

    Add material ui theme

--- client/src/config/App.tsx
+++ client/src/config/App.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 
+// Libraries
+import {ThemeProvider} from '@material-ui/core/styles';
+import {CssBaseline} from '@material-ui/core';
+
+// Config
+import theme from './theme';
+
 function App(): JSX.Element {
   return (
-    <div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
       <h1>Hello</h1>
-    </div>
+    </ThemeProvider>
   );
 }
 
