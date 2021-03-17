df212b946f5a372144010b920bcd48f8719ec1c6
Ritesh Patil <riteshsp2000@gmail.com>
2021-03-17 12:11:14 +0530

    Add eslint config

---
+++ client/.eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+  root: true,
+  extends: [
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:prettier/recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  env: {
+    browser: true,
+    jest: true,
+    node: true,
+    es6: true,
+  },
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {jsx: true},
+    tsconfigRootDir: __dirname,
+    project: './tsconfig.json',
+  },
+  settings: {
+    react: {
+      pragma: 'React',
+      version: 'detect',
+    },
+  },
+  rules: {
+    '@typescript-eslint/dot-notation': 0,
+    'no-plusplus': 0,
+  },
+}
